18c2b0f6409a5e9f83b1d64b28c087b2dc60c49e
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2024-07-13 21:46:59 -0300

    DELETE: DELTE FOLDER app/

--- app/config.py
+++
@@ -1,5 +0,0 @@
-from api.utils.db_session import URL_DB
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI=URL_DB
--- app/db/__all_models.py
+++
@@ -1,2 +0,0 @@
-from app.api.db.models.user import User
-from app.api.db.models.product import Product
